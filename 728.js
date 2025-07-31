20796e720901ce9f9e68e145ff8225e16fb521ec
hinirang <141658788+hinirang@users.noreply.github.com>
2025-07-31 12:58:57 +0800

    Add files via upload

---
+++ 728.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '3c4225e947d2aa9f4d1312e39134f851',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.highperformanceformat.com/3c4225e947d2aa9f4d1312e39134f851/invoke.js"></script>
